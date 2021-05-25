41fb80b789fff9eb4f1f03f0057592931c161461
CrazyJaffar <jaffarsadiq2132@gmail.com>
2021-05-25 13:37:53 +0530

    my blog

---
+++ _nuxt/static/1620206930/uses/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620206930",layout:"default",error:null,serverRendered:a,routePath:"\u002Fuses",config:{content:{dbHash:"690f8c1c"}},colorMode:{preference:b,value:b,unknown:a}}}(true,"system"));
